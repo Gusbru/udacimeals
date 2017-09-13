42b62e3ee1c58f1a31b2b12ddff6df1b490a0f95
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-09-13 08:40:20 -0300

    creating reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,70 @@
+import {
+  ADD_RECIPE,
+  REMOVE_FROM_CALENDAR
+} from '../actions'
+
+const initialCalendarState = {
+  sunday: {
+    breakfast: null,
+    lunch: null,
+    diner: null,
+  },
+  monday: {
+    breakfast: null,
+    lunch: null,
+    diner: null,
+  },
+  tuesday: {
+    breakfast: null,
+    lunch: null,
+    diner: null,
+  },
+  wednesday: {
+    breakfast: null,
+    lunch: null,
+    diner: null,
+  },
+  thursday: {
+    breakfast: null,
+    lunch: null,
+    diner: null,
+  },
+  friday: {
+    breakfast: null,
+    lunch: null,
+    diner: null,
+  },
+  saturday: {
+    breakfast: null,
+    lunch: null,
+    diner: null,
+  },
+}
+
+function calendar (state = initialCalendarState, action) {
+  const { day, recipe, meal } = action;
+
+  switch (action.type) {
+    case ADD_RECIPE :
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: recipe.label,
+        }
+      }
+    case REMOVE_FROM_CALENDAR :
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: null,
+        }
+      }
+    default :
+      return state
+  }
+
+}
+
+export default calendar
